78f541d3eb247456d9dc61eab4aeec6cfd13d80c
Chandrasekhargollapalli <111641344+Chandrasekhargollapalli@users.noreply.github.com>
2022-10-28 12:16:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const panels =document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+
+    panel.addEventListener('click', () => {
+     removeclasslist();
+      panel.classList.add('active');
+    })
+})
+
+function removeclasslist(){
+    panels.forEach((panel) => {
+        panel.classList.remove('active');
+    })
+
+}
